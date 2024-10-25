8f177749a8837737e46350d58fb4c51ec65d406f
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-10-25 15:01:11 -0300

    Header Responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import styles from "./Navbar.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Squash as Hamburger } from 'hamburger-react'
 import { CiUser } from "react-icons/ci";
 import HislumaLogo from "../../assets/hisluma-logo.png"
@@ -8,14 +8,52 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
   const [isOpen, setOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <nav className={styles.navbarContainer}>
-      <CiUser className={styles.userLogo} color="#fff" size={"2.5rem"} strokeWidth={"0.5px"} />
-      <Link to={"home"}>
-        <img className={styles.hislumaLogo} src={HislumaLogo} alt="Hisluma Logo" />
-      </Link>
+      {isMobile ? (
+        <>
+          <CiUser className={styles.userLogo} color="#fff" size={"2.5rem"} strokeWidth={"0.5px"} />
+          <Link to={"home"}>
+            <img className={styles.hislumaLogo} src={HislumaLogo} alt="Hisluma Logo" />
+          </Link>
+          <div className={styles.menuHamburguesa} >
+            <Hamburger toggled={isOpen} toggle={setOpen} color="#fff" />
+          </div>
 
-      <Hamburger toggled={isOpen} toggle={setOpen} color="#fff" />
+        </>
+      ) : (
+        <>
+          <Link to={"home"}>
+            <img className={styles.hislumaLogo} src={HislumaLogo} alt="Hisluma Logo" />
+          </Link>
+          <div className={styles.rightIcons}>
+            <CiUser className={styles.userLogo} color="#fff" size={"2rem"} strokeWidth={"0.5px"} />
+            <div className={styles.menuHamburguesa} >
+              <Hamburger toggled={isOpen} toggle={setOpen} color="#fff" />
+            </div>
+
+            <ul className={isMobile > 800 ? "" : styles.navBarActivacion}>
+              <li><Link>Accueil</Link></li>
+              <li><Link>Services</Link></li>
+              <li><Link>Actualités</Link></li>
+              <li><Link>Calendrier</Link></li>
+              <li><Link>à propos de nous </Link></li>
+            </ul>
+          </div>
+        </>
+      )}
     </nav>
   )
 }
