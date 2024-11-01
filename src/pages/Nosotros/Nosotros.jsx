95f9355393b5ee07be212ad190adb33ba20f9a91
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-11-01 08:41:42 -0300

    Responsive

--- src/pages/Nosotros/Nosotros.jsx
+++ src/pages/Nosotros/Nosotros.jsx
@@ -1,4 +1,4 @@
-import styles from "./Nosotros.module.css"
+import styles from "./Nosotros.module.css";
 
 import { FaLinkedin } from "react-icons/fa";
 import { FaQuoteLeft } from "react-icons/fa6";
@@ -23,25 +23,33 @@ const Nosotros = () => {
         </div>
       </header>
 
-      <article className={styles.introduccionContainer}>
-        <h2 className={styles.introduccionTitulo}>Vous avez un projet de diffusion de votre événement sur internet et vous ne savez par OÙ commencer?</h2>
-        <p className={styles.introduccionParrafo}>Hisluma est une start-up spécialisée en marketing digital et audiovisuel. Nous vous aidons à diffuser vos événements en ligne, avec promotion ciblée et création de contenu pour réseaux sociaux ou reportages. Hisluma vous accompagne pour maximiser votre visibilité sur internet.</p>
-      </article>
-
       <main className={styles.mainContainer}>
-        <h1 className={styles.mainTitulo}>À propos de Hisluma</h1>
-        <p className={styles.mainDescripcion}>
-          Hisluma est une start-up spécialisée dans le marketing sportif et les opérations audio visuelles dans ce domaine.
-          <span>Hisluma focalise son travail dans la contribution de la visibilité du sport en faisant des retransmissions d’événements sportifs soutenues par une promotion de l’événement et un suivi avec des reportages écrits et des interviews.</span>
-          <span>
-            En intérieur, ou en extérieur, une compétition locale, régionale ou nationale, vous pouvez compter sur la présence d’Hisluma dans vos événements.
-          </span>
-        </p>
+        <article className={styles.introduccionContainer}>
+          <h2 className={styles.introduccionTitulo}>Vous avez un projet de diffusion de votre événement sur internet et vous ne savez par OÙ commencer?</h2>
+          <p className={styles.introduccionParrafo}>Hisluma est une start-up spécialisée en marketing digital et audiovisuel. Nous vous aidons à diffuser vos événements en ligne, avec promotion ciblée et création de contenu pour réseaux sociaux ou reportages. Hisluma vous accompagne pour maximiser votre visibilité sur internet.</p>
+        </article>
+
+
+        <section className={styles.proposContainer}>
+          <section>
+            <h1 className={styles.mainTitulo}>À propos de Hisluma</h1>
+            <p className={styles.mainDescripcion}>
+              Hisluma est une start-up spécialisée dans le marketing sportif et les opérations audio visuelles dans ce domaine.
+              <span>Hisluma focalise son travail dans la contribution de la visibilité du sport en faisant des retransmissions d’événements sportifs soutenues par une promotion de l’événement et un suivi avec des reportages écrits et des interviews.</span>
+              <span>
+                En intérieur, ou en extérieur, une compétition locale, régionale ou nationale, vous pouvez compter sur la présence d’Hisluma dans vos événements.
+              </span>
+            </p>
+          </section>
+          <div className={styles.fotoDemo}></div>
+        </section>
       </main>
 
       <section className={styles.containerInferior}>
-        <h2 className={styles.containerInferiorTitulo}>Pourquoi « Hisluma » ?</h2>
-        <p className={styles.containerInferiorParrafo}>Le terme «macondiano» fait référence au roman Cent ans de solitude de Gabriel García Márquez. C'est de là que vient le nom «Hisluma», un diminutif de «Historias de un Luchador Macondiano», qui signifie «Histoires d’un lutteur macondiano».</p>
+        <section>
+          <h2 className={styles.containerInferiorTitulo}>Pourquoi « Hisluma » ?</h2>
+          <p className={styles.containerInferiorParrafo}>Le terme «macondiano» fait référence au roman Cent ans de solitude de Gabriel García Márquez. C'est de là que vient le nom «Hisluma», un diminutif de «Historias de un Luchador Macondiano», qui signifie «Histoires d’un lutteur macondiano».</p>
+        </section>
 
         <img className={styles.hislumaLogo} src={HislumaLogo} alt="" />
       </section>
@@ -81,17 +89,19 @@ const Nosotros = () => {
       </section>
 
       <section className={styles.nuestrosClientes}>
+        <section className={styles.nuestrosClientesContainer}>
 
-        <h2 className={styles.tituloCliente}>Nos clients</h2>
-        <p className={styles.descripcionCliente}>Rencontrez ceux qui ont fait appel à nos services et ont fait confiance à Hisluma</p>
+          <h2 className={styles.tituloCliente}>Nos clients</h2>
+          <p className={styles.descripcionCliente}>Rencontrez ceux qui ont fait appel à nos services et ont fait confiance à Hisluma</p>
 
-        <article className={styles.clientesContainer}>
-          <Clientes />
-          <Clientes />
-          <Clientes />
-          <Clientes />
-          <Clientes />
-        </article>
+          <article className={styles.clientesContainer}>
+            <Clientes />
+            <Clientes />
+            <Clientes />
+            <Clientes />
+            <Clientes />
+          </article>
+        </section>
       </section>
 
       <article className={styles.reseniasContainer}>
@@ -100,7 +110,7 @@ const Nosotros = () => {
       </article>
 
       <Formulario />
-      <Footer/>
+      <Footer />
 
     </>
   )
